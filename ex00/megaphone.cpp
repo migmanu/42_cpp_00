1494d81129be98718ffc64cc005193479fa3ea2b
Jose Manuel Migoya Bussolotti <jmanuelmigoya@gmai.com>
2024-02-12 11:40:59 +0100

    fix: not add space at end of phrase

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -13,11 +13,13 @@ int main(int argc, char *argv[])
 	for (int i = 1; i < argc; i++)
 	{
 		std::string str = argv[i];
-		for (std::string::iterator it = str.begin(); it != str.end(); it++)
+	  std::string::iterator it;
+		for (it = str.begin(); it != str.end(); it++)
 		{
-			std::cout << static_cast<char>(toupper(*it));
+			std::cout << static_cast<char>(std::toupper(*it));
 		}
-		std::cout << ' ';
+		if (*(it + 1) != '\0')
+			std::cout << ' ';
 	}
 	std::cout << std::endl;
 	return (0);
